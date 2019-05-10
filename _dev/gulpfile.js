249c00fc14ead1cd3d9f2bec25547b41125ca827
Yumetomo <yumetomo.eto@imjp.co.jp>
2019-05-10 20:27:18 +0900

    [update] スタイル適用

--- _dev/gulpfile.js
+++ _dev/gulpfile.js
@@ -17,29 +17,11 @@ task('scss', () => {
       "rem": false,
       "mqpacker": true
     }))
-    .pipe(dest('../../src/webroot/2019/'))
-})
-
-task('scss_sp', () => {
-  return src('./scss_sp/**/*.scss')
-    .pipe($.plumber({ errorHandler: notify.onError('scss_sp Error: <%= error.message %>') }))
-    .pipe($.sass({
-      importer: packageImporter({
-        extensions: ['.scss', 'css']
-      })
-    }))
-    .pipe($.pleeease({
-      "autoprefixer": { "browsers": ["last 3 versions"] },
-      "filters": false,
-      "rem": false,
-      "mqpacker": true
-    }))
-    .pipe(dest('../../src/webroot/spn/2019/'))
+    .pipe(dest('../src/assets/css/'))
 })
 
 task('watch', () => {
   watch('./scss/**/*.scss', task('scss'));
-  watch('./scss_sp/**/*.scss', task('scss_sp'));
 });
 
-task('default', series('scss', 'scss_sp', 'watch'));
+task('default', series('scss', 'watch'));
